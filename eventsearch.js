153b77d49817b2d40eb9165b396ab4e610176db7
PersephoneUnderground <persephoneunderground@users.noreply.github.com>
2018-07-03 21:07:54 -0400

    update

--- eventsearch.js
+++ eventsearch.js
@@ -1,4 +1,4 @@
-function searchEventBrite(category, subcategory, date, price, keyword) {
+function searchEventBrite(category, date, price, keyword) {
     //VARIABLE DEFINITIONS:
         
         // category = see eventbrite-ref file for all possible categories, enter only one in this case
@@ -7,7 +7,7 @@ function searchEventBrite(category, subcategory, date, price, keyword) {
         // price = "free" or "paid" or leave blank for either
         // keyword = from suggestion search
 
-        //hard-coded variables: 
+        //hard-coded variables:
             //location.address=Washington%2CDC%2CUSA - restricts search to DC region
             //expand=venue - expands the venue part of the response to allow us access to the address
             //token=QOM53KU5KI63LIHHP4CR - our access key
@@ -15,7 +15,7 @@ function searchEventBrite(category, subcategory, date, price, keyword) {
             //sort_by=best - sort the results by best match (options are “date”, “distance” and “best”)
 
     // Build the Eventbrite api query using the received parameters from the form as the inputs
-    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=Washington%2CDC%2CUSA&expand=venue&token=QOM53KU5KI63LIHHP4CR&page=1&sort_by=best&categories=" + category + "&subcategories=" + subcategory + "&start_date.keyword=" + date + "&price=" + price + "&q=" + keyword;
+    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=Washington%2CDC%2CUSA&expand=venue&token=QOM53KU5KI63LIHHP4CR&page=1&sort_by=best&categories=" + category + "&start_date.keyword=" + date + "&price=" + price + "&q=" + keyword;
     console.log(queryURL);
 
     //AJAX CALL
